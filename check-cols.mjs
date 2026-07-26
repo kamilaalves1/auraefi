27e37adcf061d938363bea4aee1de3be90decd57
Kamila Alves <kamilaalves_bg@hotmail.com>
2026-07-26 15:02:08 -0300

    chore: remove stale scripts, installers, gateway test configs and dead package.json entries

--- check-cols.mjs
+++
@@ -1,6 +0,0 @@
-import Database from 'better-sqlite3'
-const db = new Database('.data/mission-control.db')
-const info = db.prepare("PRAGMA table_info(pipeline_columns)").all()
-console.log('Columns schema:', JSON.stringify(info, null, 2))
-const cols = db.prepare('SELECT * FROM pipeline_columns ORDER BY id').all()
-console.log('Columns data:', JSON.stringify(cols, null, 2))
